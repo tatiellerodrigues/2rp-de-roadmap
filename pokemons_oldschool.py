8e67626edd28604d73c222745caed27121779bd1
tatiellerodrigues <95444453+tatiellerodrigues@users.noreply.github.com>
2022-01-10 13:18:06 -0300

    feita alteração

--- pokemons_oldschool.py
+++ pokemons_oldschool.py
@@ -2,16 +2,19 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
+#exportando arquivo csv pokemon
 df_pk = spark.table("work_dataeng.pokemon_tatielle_ext");
 
-df_pk.show();
-
-
+#exportando arquivo csv generation
 df_gene = spark.table("work_dataeng.generation_tatielle")
-df_gene.show();
 
+#filtrando as tabelas
 df_gene = df_gene.filter(df_gene["date_introduced"] < "2002-11-21")
+
+#colocando em cache
 df_gene = df_gene.cache
+
+#fazendo join
 df_gen_pok = df_pk.join(df_gene, 'generation', how='inner')
 
 df_gen_pok.write.mode('overwrite').format('orc') \
